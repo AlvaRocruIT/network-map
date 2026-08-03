c53c93cd585da49c150e901ad9ed42d0d294a5ab
AlvaHRo <alvargash90@gmail.com>
2026-08-03 19:41:04 -0400

    Update redes.js

--- redes/redes.js
+++ redes/redes.js
@@ -2280,7 +2280,7 @@ function obtenerAnguloDeterminista(valor) {
         hash = Math.imul(hash, 16777619);
     }
     return (
-        (hash >>> 0) / 4294967296) * Math.PI} * 2;
+        (hash >>> 0) / 4294967296) * Math.PI * 2;
 }
 
 function obtenerDireccionDeterminista(idA, idB) {
