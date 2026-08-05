cc398a0e91567740bf0a6ab0be87f8b439ba127e
AlvaHRo <alvargash90@gmail.com>
2026-08-04 20:19:46 -0400

    Update redes.js

--- redes/redes.js
+++ redes/redes.js
@@ -613,7 +613,8 @@ function agruparNodosPorNivelLocal(ubicacion) {
     return [...mapa.entries()]
         .sort(
             ([profundidadA], [profundidadB]) 
-             => profundidadA - profundidadB)
+             => profundidadA - profundidadB
+        )
         .map(([profundidad, nodos]) => 
          ({profundidad, nodos})
         );
